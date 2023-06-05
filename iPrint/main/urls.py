1d01b9b23a2bee81b01fb2f0e192ce38642bb867
Andres Cusirramos Marquez Mares <49249500+Andrescmm@users.noreply.github.com>
2023-06-05 02:10:49 -0500

    bootstrap

--- iPrint/main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path 
-
-from . import views
-
-urlpatterns = [
-    path("<str:name>", views.index, name="index"),
-    path("" , views.home , name="home"),
-    path("create/" , views.create , name="create"),
-]
